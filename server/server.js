bec0efd5421b5dbb391808556de4eaa59b94e32d
Sangmeshawar-4024 <nilasangameshwar425@gmail.com>
2025-07-29 18:09:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'https://finance-tracker-frontend-lac.vercel.app',
+  origin: 'https://finance-tracker-frontend-lac.vercel.app/',
   credentials: true,
 }));
 
